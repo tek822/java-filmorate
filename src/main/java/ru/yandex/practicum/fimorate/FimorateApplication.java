f1348285245aac3c79339bd4a45db95b3ce3ae57
Gusev Roman <tek822@yandex.ru>
2023-02-07 11:01:41 +0300

    feat: add maven

---
+++ src/main/java/ru/yandex/practicum/fimorate/FimorateApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.fimorate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FimorateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FimorateApplication.class, args);
+	}
+
+}
