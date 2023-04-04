2b2091e148c4b3766df72774023efc9604615e79
Gusev Roman <tek822@yandex.ru>
2023-04-04 03:05:10 +0300

    fix:
    Took 13 minutes

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -4,16 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
 @SpringBootTest
 //@AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class FilmorateApplicationTests {
-	private final UserDbStorage userDbStorage;
 
 	@Test
 	void contextLoads(){
 	}
-
 }
